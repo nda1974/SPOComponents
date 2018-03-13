4aaa66f2d62bec8a7d7febaffd46d9f7acb78124
Nicolai Danielsen <NICD@LB.DK>
2018-03-13 10:35:10 +0100

    OOB Commit

---
+++ src/webparts/manualSearchApp/components/IManualSearchAppProps.ts
@@ -0,0 +1,3 @@
+export interface IManualSearchAppProps {
+  description: string;
+}
---
+++ src/webparts/manualSearchApp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IManualSearchAppWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ManualSearchAppWebPartStrings' {
+  const strings: IManualSearchAppWebPartStrings;
+  export = strings;
+}
